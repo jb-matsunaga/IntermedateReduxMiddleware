4a2e39fc402b1f9279bdd0ff0a2da40d839e14d2
jb-matsunaga <jb.matsunaga@gmail.com>
2017-08-10 00:24:08 +0900

    update reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,9 @@
 const API_KEY = '43f7d70467359c08b8aed755c4d612d7';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: 'FETCH_WEATHER'
+    }
+}
