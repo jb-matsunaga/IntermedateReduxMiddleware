2aac62046df09b4c07e41ee835a39a38d92d24e3
jb-matsunaga <jb.matsunaga@gmail.com>
2017-08-23 01:26:36 +0900

    update app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '43f7d70467359c08b8aed755c4d612d7';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather() {
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
     return {
-        type: 'FETCH_WEATHER'
+        type: 'FETCH_WEATHER',
+        payload: request
     }
 }
